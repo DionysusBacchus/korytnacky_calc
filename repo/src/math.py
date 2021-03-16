55fb424ebdbb68f9c4c1b6caa50cea03a6648085
Jakub Sikula <xsikul15@stud.fit.vutbr.cz>
2021-03-16 15:24:04 +0100

    math.py

---
+++ repo/src/math.py
@@ -0,0 +1,23 @@
+"""@package docstring
+\file math.py
+\brief Math package for calculator
+This package contains declarations for math functions: +,-,*,/,!,^,^-1/x,... 
+"""
+def factorial(n):
+ 	if n == 0:
+		return 1
+	else:
+		return n * factorial(n - 1)
+
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+def mul(a,b)
+	return a*b
+
+def div(a,b)
+	return a/b
+
