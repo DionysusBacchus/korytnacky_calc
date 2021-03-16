286f490cabdc6539946e92e75f3516f99899b090
Jakub Sikula <xsikul15@stud.fit.vutbr.cz>
2021-03-16 15:42:21 +0100

    math.py

--- repo/src/math.py
+++ repo/src/math.py
@@ -1,17 +1,25 @@
-"""@package docstring
-\file math.py
-\brief Math package for calculator
-This package contains declarations for math functions: +,-,*,/,!,^,^-1/x,... 
-"""
+##@package
+#\file math.py
+#\brief Math package for calculator
+#This package contains declarations for math functions: +,-,*,/,!,^,^-1/x,... 
+
+## Documentation for factorial funcion.
+#
+# Takes number as argument and returns calculated factorial of given number(n=3; returns=3*2*1).
 def factorial(n):
 	if n == 0:
 		return 1
 	else:
 		return n * factorial(n - 1)
-
+## Documentation for add function
+#
+# Takes two numbers as arguments and return thier sum
 def add(a,b):
 	return a+b
 
+## Documentation for sub function 
+#
+# Takes two numbers as arguments and return thier difference
 def sub(a,b):
 	return a-b
 
