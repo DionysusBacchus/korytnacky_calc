4efd1068183b76abd99c4af382d8dfd7839581c8
Jakub Sikula <xsikul15@stud.fit.vutbr.cz>
2021-03-21 11:39:10 +0100

    Edited on call

---
+++ repo/src/mathlib.py
@@ -0,0 +1,12 @@
+##@package
+#\file math.py
+#\brief Math package for calculator
+#This package contains declarations for math functions: +,-,*,/,!,^,^-1/x,..., evalute function
+#
+
+##  
+#   
+def submit(string):
+    answer = 0
+    eval("nroot(2+5)+nroot(2,5)")
+    set_display_output(answer) 
