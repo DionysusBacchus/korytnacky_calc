cac56c2aedb190837d10c0b8e8cebed6f2d3a496
DionysusBacchus <xkhrys00@stud.fit.vutbr.cz>
2021-03-22 18:04:11 +0100

    sub function tests added

--- repo/src/tests_mathlib.py
+++ repo/src/tests_mathlib.py
@@ -39,9 +39,44 @@ class TestAdd(unittest.TestCase):
         self.assertAlmostEqual(mathlib.add(-12.43,0.43),-12,9)
         self.assertAlmostEqual(mathlib.add(4.3, -2.009),2.291,9)
     
-
+#Tests for the sub (-) function
 class TestSub(unittest.TestCase):
-    pass
+    #integer inputs
+    def test_ints_pos(self):
+        self.assertEqual(mathlib.add(5,1),4)
+        self.assertEqual(mathlib.add(1,5),-4)
+        self.assertEqual(mathlib.add(6,6),0)
+        self.assertEqual(mathlib.add(0,55687),-55687)
+        self.assertEqual(mathlib.add(167,0),167)
+    def test_ints_neg(self):
+        self.assertEqual(mathlib.add(-2,-7),5)
+        self.assertEqual(mathlib.add(-7,-2),-5)
+        self.assertEqual(mathlib.add(-4,-4),0)
+        self.assertEqual(mathlib.add(0,-18),18)
+        self.assertEqual(mathlib.add(-42,0),-42)
+    def test_ints_mixed(self):
+        self.assertEqual(mathlib.add(-4,6),-10)
+        self.assertEqual(mathlib.add(7,-2),9)
+        self.assertEqual(mathlib.add(0,0),0))
+        self.assertEqual(mathlib.add(1,-179),-180)
+        self.assertEqual(mathlib.add(87,-3),90)
+    
+    #double inputs
+    def test_doubles_pos(self):
+        self.assertAlmostEqual(mathlib.add(0.1,0.1),0,9)
+        self.assertAlmostEqual(mathlib.add(0.00000001,0.1),-0.09999999,9)
+        self.assertAlmostEqual(mathlib.add(1.234,0.234),1,9)
+        self.assertAlmostEqual(mathlib.add(0.234,1.234),-1,9)
+    def test_doubles_neg(self):
+        self.assertAlmostEqual(mathlib.add(-0.1,-0.1),0,9)
+        self.assertAlmostEqual(mathlib.add(-40.1,-3.21),-36.89,9)
+        self.assertAlmostEqual(mathlib.add(-80.77,-0.77),-80,9)
+        self.assertAlmostEqual(mathlib.add(-0.77000001,-80.77000001),80,9)
+    def test_doubles_mixed(self):
+        self.assertAlmostEqual(mathlib.add(0.1,-0.1),0.2,9)
+        self.assertAlmostEqual(mathlib.add(-0.0001,0.00000001),-0.00010001,9)
+        self.assertAlmostEqual(mathlib.add(449.5,0.001),449.499,9)
+        self.assertAlmostEqual(mathlib.add(-120.876,0.876),-120,9)
 
 class TestDiv(unittest.TestCase):
     pass
