2a0404eb916f96d3ace5d684ddc93742c19505a9
Martin Pavella <xpavel39@fit.vutbr.cz>
2021-04-20 16:17:15 +0200

    Update profiling.py

--- repo/src/profiling.py
+++ repo/src/profiling.py
@@ -29,18 +29,21 @@ for line in sys.stdin:
     line = compact_spaces(line).split(" ")
 
     for num in line:
-        data.append( float(num) )
+    	if num != "":
+		    data.append( float(num) )
 
-
-#   constants used in calculation
 data_len = len(data)
-data_average = mathlib.avg(data)
+res = 0
+if data_len != 0 and data_len != 1:
+	data_average = mathlib.avg(data)
 
-#   calculation
-sum = mathlib.sum_squares(data)
+	#   calculation
+	sum = mathlib.sum_squares(data)
 
-res = sum - data_len * (data_average**2)
-res = res * (1 / (data_len - 1))
-res = res ** 0.5
+	res = sum - data_len * (data_average**2)
+	res = res * (1 / (data_len - 1))
+	res = res ** 0.5
+elif data_len == 1:
+	res = data[0]
 
 print(res)
